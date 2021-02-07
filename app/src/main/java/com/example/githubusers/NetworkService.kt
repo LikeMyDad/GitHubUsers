ae873d1bb383a6bad768ba858ceebe7fec7fa075
Andrey <aslammany@gmail.com>
2021-02-07 22:37:43 +0400

    Add recyclerView and change code at networkservice

--- app/src/main/java/com/example/githubusers/NetworkService.kt
+++ app/src/main/java/com/example/githubusers/NetworkService.kt
@@ -1,13 +1,18 @@
 package com.example.githubusers
 
+import retrofit2.Call
+import retrofit2.Callback
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class NetworkService {
     private val BASE_URL = "https://api.github.com/"
 
-    val retrofit = Retrofit.Builder()
+    private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
+    fun <T>createService(service: Class<T>): T {
+        return retrofit.create(service)
+    }
 }
